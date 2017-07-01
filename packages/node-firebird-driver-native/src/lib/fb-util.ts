3cb1bc697e93d628ae207abd5bf8646e6063ffeb
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2017-07-01 01:20:35 -0300

    Refactor with Refactor with node-firebird-driver.

--- packages/node-firebird-driver-native/src/lib/fb-util.ts
+++ packages/node-firebird-driver-native/src/lib/fb-util.ts
@@ -1,6 +1,9 @@
 import { ClientImpl } from './client';
 import { TransactionImpl } from './transaction';
 
+import { blobInfo, getPortableInteger, sqlTypes } from 'node-firebird-driver/dist/lib/impl';
+export * from 'node-firebird-driver/dist/lib/impl';
+
 import * as fb from 'node-firebird-native-api';
 
 import * as os from 'os';
@@ -8,68 +11,6 @@ const littleEndian = os.endianness() === 'LE';
 
 import * as stringDecoder from 'string_decoder';
 
-
-/** SQL_* type constants */
-export namespace sqlTypes {
-	export const SQL_TEXT = 452;
-	export const SQL_VARYING = 448;
-	export const SQL_SHORT = 500;
-	export const SQL_LONG = 496;
-	export const SQL_FLOAT = 482;
-	export const SQL_DOUBLE = 480;
-	//export const SQL_D_FLOAT = 530;
-	export const SQL_TIMESTAMP = 510;
-	export const SQL_BLOB = 520;
-	//export const SQL_ARRAY = 540;
-	//export const SQL_QUAD = 550;
-	export const SQL_TYPE_TIME = 560;
-	export const SQL_TYPE_DATE = 570;
-	export const SQL_INT64 = 580;
-	export const SQL_BOOLEAN = 32764;
-	export const SQL_NULL = 32766;
-}
-
-/** DPB constants. */
-export namespace dpb {
-	export const lc_ctype = 48;
-	export const user_name = 28;
-	export const password = 29;
-}
-
-/** Blob info. */
-export namespace blobInfo {
-	export const totalLength = 6;
-}
-
-/** Changes a number from a scale to another. */
-/***
-export function changeScale(value: number, inputScale: number, outputScale: number): number {
-	outputScale -= inputScale;
-
-	Math.pow(10, outputScale);
-
-	if (outputScale === 0)
-		return value;
-	else if (outputScale > 0)
-		return value / Math.pow(10, outputScale);
-	else	// outputScale < 0
-		return value * Math.pow(10, -outputScale);
-}
-***/
-
-/** Emulate Firebird isc_portable_integer. */
-export function getPortableInteger(buffer: Uint8Array, length: number) {
-	if (!buffer || length <= 0 || length > 8)
-		return 0;
-
-	let value = 0;
-	let pos = 0;
-
-	for (let shift = 0; --length >= 0; shift += 8)
-		value += buffer[pos++] << shift;
-
-	return value;
-}
 
 /** Fix metadata descriptors to types we want to read. */
 export function fixMetadata(status: fb.Status, metadata: fb.MessageMetadata): fb.MessageMetadata {
