201d0773a0286da32404a034314f1a18d53d3461
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2017-07-12 16:02:52 -0300

    Refactor with node-firebird-driver-native.

--- packages/node-firebird-driver/src/lib/impl/fb-util.ts
+++ packages/node-firebird-driver/src/lib/impl/fb-util.ts
@@ -1,3 +1,8 @@
+import * as os from 'os';
+const littleEndian = os.endianness() === 'LE';
+
+import * as stringDecoder from 'string_decoder';
+
 import { ConnectOptions, CreateDatabaseOptions } from '..';
 
 
@@ -85,4 +90,278 @@ export function getPortableInteger(buffer: Uint8Array, length: number) {
 		value += buffer[pos++] << shift;
 
 	return value;
+}
+
+
+/** Decode a date. */
+export function decodeDate(date: number) {
+	let nday = date + 678882;
+	const century = Math.trunc((4 * nday - 1) / 146097);
+	nday = 4 * nday - 1 - 146097 * century;
+	let day = Math.trunc(nday / 4);
+
+	nday = Math.trunc((4 * day + 3) / 1461);
+	day = 4 * day + 3 - 1461 * nday;
+	day = Math.trunc((day + 4) / 4);
+
+	let month = Math.trunc((5 * day - 3) / 153);
+	day = 5 * day - 3 - 153 * month;
+	day = Math.trunc((day + 5) / 5);
+	let year = 100 * century + nday;
+
+	if (month < 10)
+		month += 3;
+	else {
+		month -= 9;
+		year += 1;
+	}
+
+	return { year, month, day };
+}
+
+/** Encode a date. */
+export function encodeDate(year: number, month: number, day: number): number {
+	const i = month + 9;
+	let jy = year + Math.trunc(i / 12) - 1;
+	const jm = i % 12;
+	const c = Math.trunc(jy / 100);
+	jy -= 100 * c;
+	const j = Math.trunc((146097 * c) / 4) + Math.trunc((1461 * jy) / 4) + Math.trunc((153 * jm + 2) / 5) + day - 678882;
+
+	return j;
+}
+
+/** Descode a time. */
+export function decodeTime(time: number) {
+	let seconds = Math.trunc(time / 10000);
+	let minutes = Math.trunc(seconds / 60);
+	const hours = Math.trunc(minutes / 60);
+	minutes = minutes % 60;
+	seconds = seconds % 60;
+	const fractions = time % 10000;
+
+	return { hours, minutes, seconds, fractions };
+}
+
+/** Encode a time. */
+export function encodeTime(hours: number, minutes: number, seconds: number, fractions: number): number {
+	return (hours * 3600 + minutes * 60 + seconds) * 10000 + fractions;
+}
+
+
+/** Descriptor for a field or parameter. */
+export interface Descriptor {
+	type: number;
+	subType: number;
+	length: number;
+	scale: number;
+	offset: number;
+	nullOffset: number;
+}
+
+
+export type BlobReader = (blobId: Uint8Array) => Promise<Buffer>;
+export type DataReader = (buffer: Uint8Array, blobReader: BlobReader) => Promise<any[]>;
+export type ItemReader = (buffer: Uint8Array, blobReader: BlobReader) => Promise<any>;
+
+/** Creates a data reader. */
+export function createDataReader(descriptors: Descriptor[]): DataReader {
+	const mappers = new Array<ItemReader>(descriptors.length);
+
+	for (let i = 0; i < descriptors.length; ++i) {
+		const descriptor = descriptors[i];
+
+		mappers[i] = async (buffer: Uint8Array, blobReader: BlobReader): Promise<any> => {
+			const dataView = new DataView(buffer.buffer);
+
+			if (dataView.getInt16(descriptor.nullOffset, littleEndian) == -1)
+				return null;
+
+			switch (descriptor.type) {
+				// SQL_TEXT is handled changing its descriptor to SQL_VARYING with IMetadataBuilder.
+				case sqlTypes.SQL_VARYING: {
+					//// TODO: none, octets
+					const varLength = dataView.getUint16(descriptor.offset, littleEndian);
+					const decoder = new stringDecoder.StringDecoder('utf8');
+					const buf = Buffer.from(buffer.buffer, descriptor.offset + 2, varLength);
+					return decoder.end(buf);
+				}
+
+				/***
+				case sqlTypes.SQL_SHORT:
+					return changeScale(dataView.getInt16(descriptor.offset, littleEndian), descriptor.scale, 0);
+
+				case sqlTypes.SQL_LONG:
+					return changeScale(dataView.getInt32(descriptor.offset, littleEndian), descriptor.scale, 0);
+
+				//// TODO: sqlTypes.SQL_INT64
+
+				case sqlTypes.SQL_FLOAT:
+					return dataView.getFloat32(descriptor.offset, littleEndian);
+				***/
+
+				case sqlTypes.SQL_DOUBLE:
+					return dataView.getFloat64(descriptor.offset, littleEndian);
+
+				case sqlTypes.SQL_TYPE_TIME: {
+					const now = new Date();
+					const decodedTime = decodeTime(dataView.getUint32(descriptor.offset, littleEndian));
+					return new Date(now.getFullYear(), now.getMonth(), now.getDate(),
+						decodedTime.hours, decodedTime.minutes, decodedTime.seconds, decodedTime.fractions / 10);
+				}
+
+				case sqlTypes.SQL_TYPE_DATE: {
+					const decodedDate = decodeDate(dataView.getInt32(descriptor.offset, littleEndian));
+					return new Date(decodedDate.year, decodedDate.month - 1, decodedDate.day);
+				}
+
+				case sqlTypes.SQL_TIMESTAMP: {
+					const decodedDate = decodeDate(dataView.getInt32(descriptor.offset, littleEndian));
+					const decodedTime = decodeTime(dataView.getUint32(descriptor.offset + 4, littleEndian));
+					return new Date(decodedDate.year, decodedDate.month - 1, decodedDate.day,
+						decodedTime.hours, decodedTime.minutes, decodedTime.seconds, decodedTime.fractions / 10);
+				}
+
+				case sqlTypes.SQL_BOOLEAN:
+					return dataView.getInt8(descriptor.offset) != 0;
+
+				case sqlTypes.SQL_BLOB:
+					return blobReader(buffer.slice(descriptor.offset, descriptor.offset + 8));
+
+				case sqlTypes.SQL_NULL:
+					return null;
+
+				default:
+					throw new Error(`Unrecognized Firebird type number ${descriptor.type}`);
+			}
+		}
+	}
+
+	return async (buffer: Uint8Array, blobReader: BlobReader): Promise<any[]> => {
+		const ret = new Array(descriptors.length);
+
+		for (let i = 0; i < descriptors.length; ++i)
+			ret[i] = await mappers[i](buffer, blobReader);
+
+		return ret;
+	}
+}
+
+
+export type BlobWriter = (blobId: Uint8Array, buffer: Buffer) => Promise<void>;
+export type DataWriter = (buffer: Uint8Array, values: Array<any> | undefined, blobWriter: BlobWriter) => Promise<void>;
+export type ItemWriter = (buffer: Uint8Array, values: any, blobWriter: BlobWriter) => Promise<void>;
+
+/** Creates a data writer. */
+export function createDataWriter(descriptors: Descriptor[]): DataWriter {
+	const mappers = new Array<ItemWriter>(descriptors.length);
+
+	for (let i = 0; i < descriptors.length; ++i) {
+		const descriptor = descriptors[i];
+
+		mappers[i] = async (buffer: Uint8Array, value: any, blobWriter: BlobWriter): Promise<void> => {
+			const dataView = new DataView(buffer.buffer);
+
+			if (value == null) {
+				dataView.setInt16(descriptor.nullOffset, -1, littleEndian);
+				return;
+			}
+
+			switch (descriptor.type) {
+				// SQL_TEXT is handled changing its descriptor to SQL_VARYING with IMetadataBuilder.
+				case sqlTypes.SQL_VARYING: {
+					//// TODO: none, octets
+					const str = value as string;
+					const strBuffer = Buffer.from(str);
+
+					const bytesArray = Uint8Array.from(strBuffer);
+
+					if (bytesArray.length > descriptor.length) {
+						throw new Error(`Length in bytes of string '${str}' (${bytesArray.length}) is ` +
+							`greater than maximum expect length ${descriptor.length}.`);
+					}
+
+					dataView.setUint16(descriptor.offset, bytesArray.length, littleEndian);
+
+					for (let i = 0; i < bytesArray.length; ++i)
+						buffer[descriptor.offset + 2 + i] = bytesArray[i];
+
+					break;
+				}
+
+				/***
+				case sqlTypes.SQL_SHORT:
+					dataView.setInt16(descriptor.offset, changeScale(value, 0, descriptor.scale), littleEndian);
+					break;
+
+				case sqlTypes.SQL_LONG:
+					dataView.setInt32(descriptor.offset, changeScale(value, 0, descriptor.scale), littleEndian);
+					break;
+
+				//// TODO: sqlTypes.SQL_INT64
+
+				case sqlTypes.SQL_FLOAT:
+					dataView.setFloat32(descriptor.offset, value, littleEndian);
+					break;
+				***/
+
+				case sqlTypes.SQL_DOUBLE:
+					dataView.setFloat64(descriptor.offset, value, littleEndian);
+					break;
+
+				case sqlTypes.SQL_TYPE_TIME: {
+					const date = value as Date;
+					dataView.setUint32(descriptor.offset,
+						encodeTime(date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds() * 10),
+						littleEndian);
+					break;
+				}
+
+				case sqlTypes.SQL_TYPE_DATE: {
+					const date = value as Date;
+					dataView.setInt32(descriptor.offset,
+						encodeDate(date.getFullYear(), date.getMonth() + 1, date.getDate()),
+						littleEndian);
+					break;
+				}
+
+				case sqlTypes.SQL_TIMESTAMP: {
+					const date = value as Date;
+					dataView.setInt32(descriptor.offset,
+						encodeDate(date.getFullYear(), date.getMonth() + 1, date.getDate()),
+						littleEndian);
+					dataView.setUint32(descriptor.offset + 4,
+						encodeTime(date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds() * 10),
+						littleEndian);
+					break;
+				}
+
+				case sqlTypes.SQL_BOOLEAN:
+					dataView.setInt8(descriptor.offset, value ? 1 : 0);
+					break;
+
+				case sqlTypes.SQL_BLOB:
+				{
+					const valueBuffer = value as Buffer;
+					const blobId = buffer.subarray(descriptor.offset, descriptor.offset + 8);
+					await blobWriter(blobId, valueBuffer);
+					break;
+				}
+
+				case sqlTypes.SQL_NULL:
+					break;
+
+				default:
+					throw new Error(`Unrecognized Firebird type number ${descriptor.type}`);
+			}
+		}
+	}
+
+	return async (buffer: Uint8Array, values: Array<any>, blobWriter: BlobWriter): Promise<void> => {
+		if ((values || []).length !== descriptors.length)
+			throw new Error(`Incorrect number of parameters: expected ${descriptors.length}, received ${(values || []).length}.`);
+
+		for (let i = 0; i < descriptors.length; ++i)
+			await mappers[i](buffer, values[i], blobWriter);
+	}
 }
